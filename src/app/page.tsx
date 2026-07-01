2279050f7c361772b340f3c2b20a9c41126c78f2
biitassets-VSS <biit.assets@gmail.com>
2026-07-01 13:10:09 +0530

    Staff root update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function RootPage() {
-  // Automatically redirect the root URL to the new Admin login page
-  redirect('/login/admin');
+  // Automatically redirect the root URL to the new staff login page
+  redirect('/login/staff');
 }
