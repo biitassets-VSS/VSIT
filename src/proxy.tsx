56a225d934abf328ca79f5db2d0de6d1c64f9c45
biitassets-VSS <biit.assets@gmail.com>
2026-07-21 12:42:50 +0530

    upden

--- src/proxy.tsx
+++ src/proxy.tsx
@@ -1,8 +1,6 @@
 import { NextResponse, type NextRequest } from 'next/server'
 import { createServerClient } from '@supabase/ssr'
 
-export const runtime = 'edge';
-
 export default async function middleware(request: NextRequest) {
   let response = NextResponse.next({
     request: {
